3a6d346f91516e9cbbe31b337810b1683ed43019
Aditya Pandey <adityapandeyad@outlook.com>
2025-12-17 15:53:01 +0530

    [Aditya Pandey] reverse solution

---
+++ core-java-practice/leet-code-codebase/reverse.java
@@ -0,0 +1,17 @@
+class reverse {
+    public int reverseno(int x) {
+        long output = 0;
+
+        while(x!=0){
+            int digit=x%10;
+            output+=digit;
+            x/=10;
+            output *=10;
+        }
+        output/=10;
+        if(output<Integer.MIN_VALUE || output>Integer.MAX_VALUE){
+            return 0;
+        }
+        return (int)output;
+    }
+}
