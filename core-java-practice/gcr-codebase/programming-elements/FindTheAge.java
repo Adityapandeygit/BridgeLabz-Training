2a342427d3355ff30a22930ee5ed615d4d460a4b
Aditya Pandey <adityapandeyad@outlook.com>
2025-12-18 11:11:41 +0530

    [Aditya Pandey] added practice problem in gcr-code-base

---
+++ core-java-practice/gcr-codebase/programming-elements/FindTheAge.java
@@ -0,0 +1,9 @@
+package gcr_codebase;
+
+public class FindTheAge {
+	public static void main(String[] args) {
+		int birth_year = 2000;
+		int curr_year = 2024;
+		System.out.print("Harry's age in 2024" + (curr_year-birth_year));
+	}
+}
---
+++ core-java-practice/gcr-codebase/programming-elements/KilometersToMiles.java
@@ -0,0 +1,8 @@
+package gcr_codebase;
+
+public class KilometersToMiles {
+	public static void main(String[] args) {
+		double dis_in_km = 10.8;
+		System.out.println("The distence 10.8km in miles is "+(dis_in_km*1.6));
+	}
+}
