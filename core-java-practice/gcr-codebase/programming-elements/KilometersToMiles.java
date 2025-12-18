f37d062cd36e523a87f7fc1ce3c39cf4dc3b5eb8
Aditya Pandey <adityapandeyad@outlook.com>
2025-12-18 16:49:51 +0530

    Remove duplicate Level 1 Java files

--- core-java-practice/gcr-codebase/programming-elements/KilometersToMiles.java
+++
@@ -1,8 +0,0 @@
-package gcr_codebase;
-
-public class KilometersToMiles {
-	public static void main(String[] args) {
-		double dis_in_km = 10.8;
-		System.out.println("The distence 10.8km in miles is "+(dis_in_km*1.6));
-	}
-}
