096b60f94a23ef4cdd13bbafefb041f12f73299d
Aditya Pandey <adityapandeyad@outlook.com>
2025-12-18 11:48:09 +0530

    [Aditya Pandey] Added Programming Elements practice solution

---
+++ core-java-practice/gcr-codebase/programming-elements/DividingPens.java
@@ -0,0 +1,10 @@
+package gcr_codebase;
+
+public class DividingPens {
+public static void main(String[] args) {
+	int pens =14;
+	int student = 3;
+	int nonDist = 14%3;
+	System.out.println("The Pen per student is "+(pens/student)+"and the remaining Pen not distributed "+nonDist);
+}
+}
