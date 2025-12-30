944128c938a2f4400ab4cb873fe587b919d706cd
Aditya Pandey <adityapandeyad@outlook.com>
2025-12-30 16:38:34 +0530

    [Aditya Pandey] Add .Added Employee program in class and object

---
+++ oops-practice/gcr-codebase/class-and-object/Employee.java
@@ -0,0 +1,22 @@
+package classandobject;
+
+public class Employee {
+	String name;
+    int id;
+    double salary;
+    
+    void displayDetails() {
+        System.out.println("Employee Name: " + name);
+        System.out.println("Employee id: " + id);
+        System.out.println("Employee Salary: " + salary);
+    }
+    public static void main(String[] args) {
+        // Creating an object of Employee class 
+        Employee emp = new Employee();
+        // Assigning values
+        emp.name = "Rohan";
+        emp.id = 1;
+        emp.salary = 500000;
+        emp.displayDetails();
+    }
+}
